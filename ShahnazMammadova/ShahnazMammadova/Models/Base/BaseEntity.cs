d02139c339c479d92fc6f052e4f8e35f06dc9143
ramalfatullayevh <tu7ma9clt@code.edu.az>
2023-08-21 15:22:50 +0400

    feat: added database connection

---
+++ ShahnazMammadova/ShahnazMammadova/Models/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace ShahnazMammadova.Models.Base
+{
+	public class BaseEntity
+	{
+        public int Id { get; set; }
+        public DateTime CreatedTime { get; set; } 
+        public DateTime? UpdatedTime { get; set; } 
+        public DateTime? DeletedTime { get; set; } 
+        public bool IsDeleted { get; set; }         
+    }
+}
--- ShahnazMammadova/ShahnazMammadova/Program.cs
+++ ShahnazMammadova/ShahnazMammadova/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ShahnazMammadova.DataAccessLayer.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDBContext>(opt =>
+{
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("Connection"));
+});
 
 var app = builder.Build();
 
