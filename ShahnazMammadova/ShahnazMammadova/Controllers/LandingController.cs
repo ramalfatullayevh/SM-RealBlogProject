d92bf428b04974613386057475a8656d4816a1bd
ramalfatullayevh <tu7ma9clt@code.edu.az>
2023-08-25 16:55:06 +0400

    feat: created landing blogs and stories part

--- ShahnazMammadova/ShahnazMammadova/Controllers/LandingController.cs
+++ ShahnazMammadova/ShahnazMammadova/Controllers/LandingController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShahnazMammadova.DataAccessLayer.Context;
+using ShahnazMammadova.ViewModels;
 
 namespace ShahnazMammadova.Controllers
 {
     public class LandingController : Controller
     {
-        public IActionResult Index()
+        readonly AppDBContext _context;
+
+        public LandingController(AppDBContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            LandingVM landing = new LandingVM
+            {
+                Blogs = await _context.Blogs.Include(b=>b.Category).Where(b => b.IsDeleted == false).Where(b => b.IsPopular == true).OrderByDescending(b => b.CreatedTime).ToListAsync(),
+                Stories = await _context.Stories.Include(b=>b.Category).Where(b => b.IsDeleted == false).Where(b => b.IsPopular == true).OrderByDescending(b => b.CreatedTime).ToListAsync(),
+            };
+            return View(landing);
         }
     }
 }
---
+++ ShahnazMammadova/ShahnazMammadova/ViewModels/Landing/LandingVM.cs
@@ -0,0 +1,10 @@
+﻿using ShahnazMammadova.Models;
+
+namespace ShahnazMammadova.ViewModels
+{
+    public class LandingVM
+    {
+        public ICollection<Blog> Blogs { get; set; }
+        public ICollection<Story> Stories { get; set; }
+    }
+}
