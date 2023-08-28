b6a8a031e2d6abdc4016da05be393efc323103ea
ramalfatullayevh <tu7ma9clt@code.edu.az>
2023-08-28 12:56:17 +0400

    feat : created contact page

--- ShahnazMammadova/ShahnazMammadova/Controllers/ContactController.cs
+++ ShahnazMammadova/ShahnazMammadova/Controllers/ContactController.cs
@@ -1,11 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShahnazMammadova.DataAccessLayer.Context;
+using ShahnazMammadova.Models;
+using ShahnazMammadova.ViewModels;
 
 namespace ShahnazMammadova.Controllers
 {
 	public class ContactController : Controller
 	{
-		public IActionResult Index()
+		readonly AppDBContext _context;
+		readonly UserManager<User> _userManager;
+		public ContactController(AppDBContext context, UserManager<User> userManager)
 		{
+			_context = context;
+			_userManager = userManager;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(ContactVM vM)
+		{
+			var userid = _userManager.GetUserId(HttpContext.User);
+			var user = await _userManager.FindByIdAsync(userid);
+			var contact = new Contact();
+			if (user is null) 
+			{
+				contact = new Contact
+				{
+					CreatedDate = DateTime.Now,	
+					Name = vM.Name,
+					Email = vM.Email,	
+					Subject = vM.Subject,
+					Message = vM.Message,	
+				};
+			}
+			else 
+			{
+				contact = new Contact
+				{
+					CreatedDate = DateTime.Now,
+					Name = user.Name,
+					Email = user.Email,
+					Subject = vM.Subject,
+					Message = vM.Message,
+				};
+			}
+			await _context.Contacts.AddAsync(contact);	
+			await _context.SaveChangesAsync();
+			ModelState.Clear();
+			ViewBag.Message = "Mesajınız uğurla göndərildi";
 			return View();
 		}
 	}
